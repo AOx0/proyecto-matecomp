ef417621dff3577c5e3da92a99edbe7d978791d6
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-11-16 16:04:01 -0600

    Improve bis

--- src/bin/bis.rs
+++ src/bin/bis.rs
@@ -1,52 +1,63 @@
-use fraction::Signed;
+use std::f64::consts::E;
 
-type F = fraction::BigFraction;
+use proyecto_matecomp::MAX_ITER;
 
-struct BM {
-    a: F,
-    b: F,
-    t: F,
+fn main() {
+    let mut p = 10.;
+    let solver = BM::new(&|x| E.powf(3. * x) - 4., (0., 1.)).with_iter(MAX_ITER);
+
+    for (i, s) in solver.into_iter().enumerate() {
+        if s == p {
+            println!("Equal sol iter {i}, val:\n{:.60}", p);
+            break;
+        }
+        println!("{}: {:.60}", i, s);
+        p = s;
+    }
+}
+
+struct BM<'a> {
+    f: &'a dyn Fn(f64) -> f64,
+    a: f64,
+    b: f64,
     iter: Option<usize>,
 }
 
-impl BM {
-    fn new(a: F, b: F) -> Self {
+impl<'a> BM<'a> {
+    fn new(f: &'a dyn Fn(f64) -> f64, (a, b): (f64, f64)) -> Self {
         Self {
+            f,
             a,
             b,
-            t: F::new(0u64, 1u64),
             iter: None,
         }
     }
+
     fn with_iter(mut self, iter: usize) -> Self {
         self.iter = Some(iter);
         self
     }
 }
 
-impl Iterator for BM {
-    type Item = F;
+impl<'a> Iterator for BM<'a> {
+    type Item = f64;
     fn next(&mut self) -> Option<Self::Item> {
-        matches!(self.iter, Some(x) if x > 0).then(|| {
-            *self.iter.as_mut().unwrap() -= 1;
-            self.t = (self.a.clone() + self.b.clone()) / F::new(2u64, 1u64);
-            let ft = self.t.clone() * self.t.clone() - F::new(3u64, 1u64);
-            if ft.clone() * (self.a.clone() * self.t.clone() - F::new(3u64, 1u64))
-                < F::new(0u64, 1u64)
-            {
-                self.b = self.t.clone();
-            } else {
-                self.a = self.t.clone();
-            }
-            self.t.clone()
-        })
-    }
-}
+        matches!(self.iter, Some(x) if x > 0)
+            .then(|| {
+                *self.iter.as_mut().unwrap() -= 1;
+                let m = (self.a + self.b) / 2.;
+                let ins = [self.a, m, self.b];
+                let pos = ins
+                    .iter()
+                    .enumerate()
+                    .find_map(|(i, &v)| ((self.f)(v) >= 0. && i > 0).then_some(i));
 
-fn main() {
-    let mut solver = BM::new(F::new(1u64, 1u64), F::new(2u64, 1u64)).with_iter(7);
-
-    for (i, j) in solver.into_iter().enumerate() {
-        println!("{}", j);
+                pos.and_then(|i| {
+                    self.a = ins[i - 1];
+                    self.b = ins[i];
+                    Some(m)
+                })
+            })
+            .flatten()
     }
 }
