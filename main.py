34a69b6d48ea986827ad754f41bc5fb703dff4b0
Lorenzo Reinoso Fuentes <0212511@up.edu.mx>
2023-11-22 14:07:40 -0600

    sympy implementation for func interpretation and derivation

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import streamlit as st
 from sympy import symbols, Eq
-x, y, z = symbols('x y z')
+
 
 st.write("# Resuelve tu ecuación")
 
-func = st.text_input("Introduce tu función")
+func = st.text_input("Introduce tu ecuación")
 metodo = st.selectbox('Selecciona un método', ["Bisección", "NEWTON-RAPHSON", "SECANTE"])
 
-if st.button("Arre!"):
+if st.button("resuelve!"):
     if metodo == "Bisección":
         st.latex(r''' m = \frac{a+b}{2} ''')
+        st.code('for i in range(8): foo()')
     elif metodo == "NEWTON-RAPHSON":
         st.latex(r''' X_{n+1} = X_{n} - \frac{f(X_n)}{f'(X_n)}''')
     elif metodo == "SECANTE":
         st.latex(r''' X_{n+1} = X_{n} - \frac{X_n-X_{n-1}}{f(X_n)-f(X_{n-1})}f(X_n)''')
-
+    x = symbols('x')
+    y = eval(func)
+    yprima = y.diff(x)
+    st.write(y)
+    st.write(yprima)
     st.write(metodo)
 with st.sidebar:
     st.write("# Equipo 1")
